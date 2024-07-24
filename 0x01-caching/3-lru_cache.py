1d6f2a6617dbef6e53a6a1cd0a9419a22393237d
TevinDeja <tevinowena@gmail.com>
2024-07-24 10:02:46 +0000

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""LRUCache module"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRUCache defines a LRU caching system
+    """
+
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.usage_order = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    lru_key = self.usage_order.pop(0)
+                    del self.cache_data[lru_key]
+                    print(f"DISCARD: {lru_key}")
+            
+            self.cache_data[key] = item
+            self._update_usage(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            self._update_usage(key)
+            return self.cache_data[key]
+        return None
+
+    def _update_usage(self, key):
+        """
+        Update the usage order of the keys
+        """
+        if key in self.usage_order:
+            self.usage_order.remove(key)
+        self.usage_order.append(key)
