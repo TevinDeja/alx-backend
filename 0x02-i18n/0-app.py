2549b4aaa6925992585f92c6f2956be7e9e66ddc
TevinDeja <tevinowena@gmail.com>
2024-07-30 17:20:04 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,8 @@ Basic Flask app
 from flask import Flask, render_template
 
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 
 
 @app.route('/')
@@ -19,5 +17,5 @@ def get_index() -> str:
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
